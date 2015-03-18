ed9c559e9fcf124a0c5f4627ff1c45285971bab4
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-03-18 17:42:13 +0100

    Fixed tmp main id incrementation

--- main.cpp
+++ main.cpp
@@ -17,18 +17,18 @@ void testJuliaLocal(void)
 	const unsigned int H = 42;
 
 	const Quaternion C(-0.835, -0.232, 0, 0);
-
+	
 	sf::Uint32 id = 0;
 	SuiteCollection suites;
 	Fractal fractal;
 	FractalViewConsole view(&fractal, W, H);
 
-	for(unsigned int x = 0; x < W; ++x)
+	for(unsigned int y = 0; y < H; ++y)
 	{
-		for(unsigned int y = 0; y < H; ++y)
+		for(unsigned int x = 0; x < W; ++x)
 		{
-			Quaternion z0((x-W/2.)/100., (y-H/2.)/100., 0, 0);
-			suites.push_back(new QS_Julia(id++, z0, C, 100., 100.));
+			Quaternion z0((x-W/2.)/20., (y-H/2.)/20., 0, 0);
+			suites.push_back(new QS_Julia(id++, z0, C, 50., 100.));
 		}
 	}
 
