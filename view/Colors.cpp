eb07285e854dfb7b1d16e41b0e8a5312cf69c2a9
slallement <serguei.l@orange.fr>
2015-03-19 13:58:57 +0100

    Add of the Colors class

---
+++ view/Colors.cpp
@@ -0,0 +1,46 @@
+#include "Colors.h"
+
+/* HSV to RGB conversion */
+void Colors::HsvToRgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, unsigned char s, unsigned char v)
+{
+    unsigned char region, fpart, p, q, t;
+    if(s == 0) { /* grayscale */
+        *r = *g = *b = v;
+        return;
+    }
+    
+    /* make hue 0-5 */
+    region = h / 43;
+    /* find remainder part, make it from 0-255 */
+    fpart = (h - (region * 43)) * 6;
+    
+    /* calculate temp vars, doing integer multiplication */
+    p = (v * (255 - s)) >> 8;
+    q = (v * (255 - ((s * fpart) >> 8))) >> 8;
+    t = (v * (255 - ((s * (255 - fpart)) >> 8))) >> 8;
+        
+    /* assign temp vars based on color cone region */
+    switch(region) {
+        case 0:
+            *r = v; *g = t; *b = p; break;
+        case 1:
+            *r = q; *g = v; *b = p; break;
+        case 2:
+            *r = p; *g = v; *b = t; break;
+        case 3:
+            *r = p; *g = q; *b = v; break;
+        case 4:
+            *r = t; *g = p; *b = v; break;
+        default:
+            *r = v; *g = p; *b = q; break;
+    }
+}
+
+Colors::Colors()
+{
+}
+
+Colors::~Colors()
+{
+}
+
